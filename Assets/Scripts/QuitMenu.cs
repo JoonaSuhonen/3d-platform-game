dc4148910e4d11e26e75ab3a1d1ffeda622a3719
Joona Suhonen <mrangryjoe@gmail.com>
2021-11-16 11:05:39 +0200

    Menu functions implemented

---
+++ Assets/Scripts/QuitMenu.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+public class QuitMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void ExitGame(){
+        
+        Application.Quit();
+    }
+    
+}

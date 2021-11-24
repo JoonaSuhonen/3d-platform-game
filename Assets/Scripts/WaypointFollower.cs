4c94b0cae4f7b68ae377dabf667f6f7294bbd149
Joona Suhonen <59951268+JoonaSuhonen@users.noreply.github.com>
2021-11-24 20:54:59 +0200

    Player movement with camera

--- Assets/Scripts/WaypointFollower.cs
+++ Assets/Scripts/WaypointFollower.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class WaypointFollower : MonoBehaviour
 {
     [SerializeField] GameObject[] waypoints;
-    int currentWaypointIndex = 0;
     [SerializeField] float speed = 1f;
+    int currentWaypointIndex = 0;
+    
     void Update()
     {
         if (Vector3.Distance(transform.position, waypoints[currentWaypointIndex].transform.position) < .1f)
